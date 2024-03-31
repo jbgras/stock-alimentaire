91a1a965d14b744e3be867ce4ecb0a9d0c40a317
jbgras <jbgras@hotmail.fr>
2024-03-31 09:06:54 -0700

    debug: useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Table from './Table.js'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, getDocs, connectFirestoreEmulator } from 'firebase/firestore/lite';
+import React, { useState }  from 'react';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDDQbjNHCL7TtYLSmbqIZI3a8w19W-xbnk",
+  authDomain: "la-boussole-vancouver.firebaseapp.com",
+  projectId: "la-boussole-vancouver",
+  storageBucket: "la-boussole-vancouver.appspot.com",
+  messagingSenderId: "305758368045",
+  appId: "1:305758368045:web:a0420abb77215d03d9a3a4",
+  measurementId: "G-8CXML2B1VH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+//connectFirestoreEmulator(db, 'localhost', 8080); // emulator
+
+// Get a list of articles from your database
+async function GetArticles() {
+  // const [articles, setArticles] = useState()
+  const [articles, setArticles] = useState();
+
+  const articlesCol = collection(db, 'articles');
+  const articlesSnapshot = await getDocs(articlesCol);
+  const articlesList = articlesSnapshot.docs.map(doc => doc.data());
+  // // .then((articlesSnapshot) => {
+  // //     articles = articlesSnapshot.docs.map(doc => doc.data())
+  // // })
+  // // .catch ((error) => console.log(error) )
+  setArticles(articlesList);
+  console.log("articles:"+JSON.stringify(articles))
+}
 
 function App() {
+  GetArticles()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React JB 
-        </a>
-      </header>
+          table : <Table/>
     </div>
 );
 }
--- src/index.js
+++ src/index.js
@@ -3,40 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDocs, connectFirestoreEmulator } from 'firebase/firestore/lite';
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDDQbjNHCL7TtYLSmbqIZI3a8w19W-xbnk",
-  authDomain: "la-boussole-vancouver.firebaseapp.com",
-  projectId: "la-boussole-vancouver",
-  storageBucket: "la-boussole-vancouver.appspot.com",
-  messagingSenderId: "305758368045",
-  appId: "1:305758368045:web:a0420abb77215d03d9a3a4",
-  measurementId: "G-8CXML2B1VH"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
-//connectFirestoreEmulator(db, 'localhost', 8080); // emulator
-
-// Get a list of articles from your database
-async function getArticles(db) {
-  const articlesCol = collection(db, 'articles');
-  const articlesSnapshot = await getDocs(articlesCol);
-  const articlesList = articlesSnapshot.docs.map(doc => doc.data());
-  return articlesList;
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
-const articlesList = await getArticles(db)
-console.log("articles:"+JSON.stringify(articlesList))
 root.render(
   <React.StrictMode>
     <App />
